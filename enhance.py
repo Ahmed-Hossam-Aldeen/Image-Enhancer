1c043c58488acab5768a6aeda3ad0224d3a0d38e
Ahmed_Hossam <61332730+Ahmed-Hossam-Aldeen@users.noreply.github.com>
2022-02-26 17:13:25 +0200

    added LapSRN algorithm

--- enhance.py
+++ enhance.py
@@ -17,6 +17,7 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__()
         uic.loadUi('enhance.ui', self)
         self.actionadd_image.triggered.connect(self.openFileNameDialog) 
+        self.checkBox.stateChanged.connect(self.show_original)
         self.enhance.clicked.connect(self.enhancez)
         self.setWindowTitle("Enhancer!")
         self.show() 
@@ -26,11 +27,25 @@ class MainWindow(QtWidgets.QMainWindow):
         if path != ('', ''):
             self.path = path[0]
             self.name = os.path.basename(self.path)
+        pixmap = QPixmap(self.path)
+        self.label.setPixmap(pixmap)
+        self.label.setScaledContents(True)     
         print(self.path)    
         print(self.name)
-            
+        
+    def show_original(self): 
+        if self.checkBox.isChecked():
+            pixmap = QPixmap(self.path)
+            self.label.setPixmap(pixmap)
+            self.label.setScaledContents(True)
+        else:
+            pixmap = QPixmap(self.output)
+            self.label.setPixmap(pixmap)
+            self.label.setScaledContents(True)          
+        
     def enhancez(self):
         self.label.clear()
+        self.checkBox.setChecked(False)
         # Create an SR object
         sr = dnn_superres.DnnSuperResImpl_create()
         
@@ -38,29 +53,42 @@ class MainWindow(QtWidgets.QMainWindow):
         if self.ESPCN_x2.isChecked():
             path = "./models/ESPCN_x2.pb"
             sr.setModel("espcn", 2)
-            output = f"./ESPCN_x2_{self.name}"           
+            self.output = f"./ESPCN_x2_{self.name}"           
         elif self.ESPCN_x3.isChecked():
             path = "./models/ESPCN_x3.pb"
             sr.setModel("espcn", 3)
-            output = f"./ESPCN_x3_{self.name}"
+            self.output = f"./ESPCN_x3_{self.name}"
         elif self.ESPCN_x4.isChecked():
             path = "./models/ESPCN_x4.pb"
             sr.setModel("espcn", 4)  
-            output = f"./ESPCN_x4_{self.name}"
+            self.output = f"./ESPCN_x4_{self.name}"
             
             
         elif self.FSRCNN_x2.isChecked():
             path = "./models/FSRCNN_x2.pb"
             sr.setModel("fsrcnn", 2)  
-            output = f"./FSRCNN_x2_{self.name}"            
+            self.output = f"./FSRCNN_x2_{self.name}"            
         elif self.FSRCNN_x3.isChecked():
             path = "./models/FSRCNN_x3.pb"
             sr.setModel("fsrcnn", 3)  
-            output = f"./FSRCNN_x3_{self.name}"            
+            self.output = f"./FSRCNN_x3_{self.name}"            
         elif self.FSRCNN_x4.isChecked():
             path = "./models/FSRCNN_x4.pb"
             sr.setModel("fsrcnn", 4)  
-            output = f"./FSRCNN_x4_{self.name}"
+            self.output = f"./FSRCNN_x4_{self.name}"
+
+        elif self.LapSRN_x2.isChecked():
+            path = "./models/LapSRN_x2.pb"
+            sr.setModel("lapsrn", 2)  
+            self.output = f"./LapSRN_x2_{self.name}"            
+        elif self.LapSRN_x4.isChecked():
+            path = "./models/LapSRN_x4.pb"
+            sr.setModel("lapsrn", 4)  
+            self.output = f"./LapSRN_x4_{self.name}"            
+        elif self.LapSRN_x8.isChecked():
+            path = "./models/LapSRN_x8.pb"
+            sr.setModel("lapsrn", 8)  
+            self.output = f"./LapSRN_x8_{self.name}"
             
         # Read image
         image = cv2.imread(self.path)
@@ -70,8 +98,8 @@ class MainWindow(QtWidgets.QMainWindow):
         result = sr.upsample(image)
         print("done")
         # Save the image
-        cv2.imwrite(output, result)
-        pixmap = QPixmap(output)
+        cv2.imwrite(self.output, result)
+        pixmap = QPixmap(self.output)
         self.label.setPixmap(pixmap)
         self.label.setScaledContents(True)  
     
